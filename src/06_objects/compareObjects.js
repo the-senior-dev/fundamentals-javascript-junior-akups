bffc066a152521077000a901fa2d4a9328a96300
akups <appiah-lindberg@outlook.com>
2025-03-06 17:28:18 +0100

    comparing objects

--- src/06_objects/compareObjects.js
+++ src/06_objects/compareObjects.js
@@ -1,13 +1,14 @@
 /**
  * Write a function that makes a deep comparison between two objects.
- * Deep Comparison: two values are only considered equal if they are the same value or are objects with the same 
+ * Deep Comparison: two values are only considered equal if they are the same value or are objects with the same
  * properties whose values are also equal when compared with a recursive call to deepEqual.
- * @param {*} obj1 
- * @param {*} obj2 
+ * @param {*} obj1
+ * @param {*} obj2
  */
 function compareObjects(obj1, obj2) {
-    // Your code here
+  if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+    return true;
+  } else return false;
 }
-  
+
 module.exports = compareObjects;
-  
